e420640af3bbbbdecd0e3a591b10d1b09fb17869
Matheus de Farias Cavalcanti Santos <matheusdefariascs@gmail.com>
2021-08-26 20:32:47 -0300

    Uploading files

---
+++ Section-2/assignments/assignment#1/assignment.js
@@ -0,0 +1,11 @@
+let result;
+const userInput = 34;
+
+result = 18 + userInput;
+
+result = result - 2;
+result = result * 2;
+result = result / 5;
+
+alert('Result: ' + result);
+alert('User Input: ' + userInput);
---
+++ Section-4/the-unconventional-calculator/assets/scripts/app.js
@@ -0,0 +1,78 @@
+// Section 2 - The Unconventional Calculator
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+// Get input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+// Provide information about the operations executed
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function calculateResult(calculationType) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === "ADD") {
+    currentResult += enteredNumber;
+    mathOperator = "+";
+  } else if (calculationType === "SUBTRACT") {
+    currentResult -= enteredNumber;
+    mathOperator = "-";
+  } else if (calculationType === "MULTIPLY") {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (calculationType === "DIVIDE") {
+    currentResult /= enteredNumber;
+    mathOperator = "/";
+  }
+
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+/*
+The following functions are related to arithmetic operations
+- Addition - add()
+- Subtraction - subtract()
+- Multiplication - multiply()
+- Division - divide()
+*/
+function add() {
+  calculateResult("ADD");
+}
+
+function subctract() {
+  calculateResult("SUBTRACT");
+}
+
+function multiply() {
+  calculateResult("MULTIPLY");
+}
+
+function divide() {
+  calculateResult("DIVIDE");
+}
+
+// Buttons actions
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subctract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
