fff9c35680aab830ca657f0b84dc6db41bb8beee
Matheus de Farias Cavalcanti Santos <matheusdefariascs@gmail.com>
2021-09-08 17:55:04 -0300

    feat: add monster killer project to section 4

--- Section-4/the-unconventional-calculator/assets/scripts/app.js
+++ Section-4/the-unconventional-calculator/assets/scripts/app.js
@@ -29,6 +29,15 @@ function writeToLog(operationIdentifier, prevResult, operationNumber, newResult)
 
 function calculateResult(calculationType) {
   const enteredNumber = getUserNumberInput();
+  if (calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' &&
+    calculationType !== 'DIVIDE' ||
+    !enteredNumber
+  ) {
+    return;
+  }
+
   const initialResult = currentResult;
   let mathOperator;
   if (calculationType === "ADD") {
